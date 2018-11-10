673135dad67474013633df0f41da00bb3aae2268
Orlewilson B. Maia <orlewilson.maia@gmail.com>
2018-11-10 02:03:09 -0400

    adicao hello world console

---
+++ 01-hello-world/app.js
@@ -0,0 +1,10 @@
+/*
+  Como Criar Coisas Inteligentes com Arduino - Módulo 3
+  Facilitador:       Prof. Orlewilson Bentes Maia
+  Data Criacao:      09/11/2018
+  Data Atualizacao:  10/11/2018
+  Descricao:         Hello World no console
+*/
+
+// mostra mensagem no console do NodeJS
+console.log("Hello World, NodeJS!");
