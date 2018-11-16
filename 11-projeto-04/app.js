c3f8b1c1efe8c28d193e93c7be99047562d13c51
Orlewilson B. Maia <orlewilson.maia@gmail.com>
2018-11-15 22:10:29 -0400

    adicao do projeto 04

---
+++ 11-projeto-04/app.js
@@ -0,0 +1,41 @@
+/*
+  Como Criar Coisas Inteligentes com Arduino - Módulo 3
+  Facilitador:       Prof. Orlewilson Bentes Maia
+  Data Criacao:      15/11/2018
+  Data Atualizacao:  16/11/2018
+  Descricao:         Projeto 04: Serviço para tocar várias melodias
+
+*/
+
+// importando bibliotecas
+
+// biblioteca para comunicar com o Arduino
+var five = require("johnny-five");  
+
+// biblioteca de melodias
+var listaMuiscas = require('j5-songs');
+
+// variáveis
+// representa a placa arduino
+var board = new five.Board({port: "COM14"});
+
+// representa o piezo
+var piezo;
+
+// representa a musica
+var musica;
+
+// quando a placa estiver pronta, execute.
+board.on("ready", function() {  
+   
+   // inicializa o piezo na porta 3 
+   piezo = new five.Piezo(3);
+
+   // escolhe a música
+   // exemplos de musicas: mario-fanfare, mario-intro, starwars-theme
+   musica = listaMuiscas.load("mario-fanfare");
+   
+   // toca a musica
+   console.log("tocando a musica...");
+   piezo.play(musica);
+});
