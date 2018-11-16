b76cefe9aa3d24476755e76f2fca55a1f069e544
Orlewilson B. Maia <orlewilson.maia@gmail.com>
2018-11-16 00:00:59 -0400

    atualizacao do projeto 06 web

--- 17-projeto-06-web/app.js
+++ 17-projeto-06-web/app.js
@@ -3,22 +3,68 @@
   Facilitador:       Prof. Orlewilson Bentes Maia
   Data Criacao:      15/11/2018
   Data Atualizacao:  16/11/2018
-  Descricao:         Projeto 03: Serviço para alertar sobre 
-                     proximidade de objetos
+  Descricao:         Projeto 06: Serviço para mostrar informações
+  					         de uma mini estação meteorológica
 
 */
 
 // importando bibliotecas
 
-// biblioteca para comunicar com o Arduino
-var five = require("johnny-five");  
+// para trabalhar com páginas web
+var app = require('express')();
 
-// informando a porta de comunicação
-var board = new five.Board({port: "COM14"});
- 
+//  para transferir dados por meio do protocolo HTTP
+var server = require('http').Server(app);
+
+// para criar conexão socket
+var io = require('socket.io')(server);
+
+// para saber ip da máquina
+var ip = require("ip");  
+
+// para acesso serial a placa Arduino
+var SerialPort = require('serialport');
+const Readline = require('@serialport/parser-readline')
+
+// abre a porta de comunicação
+var port = new SerialPort('COM14', {
+	baudRate: 9600
+});
+
 // variáveis
 
-// quando a placa estiver pronta, execute.
-board.on("ready", function() {  
-   
+// formatador de conteúdo
+const parser = port.pipe(new Readline({delimiter: '\r\n'}))
+
+
+// servidor escutando na porta 8080
+server.listen(8080);
+// mensagem no console
+console.log("Digite no seu navegador http://"+ ip.address() + ":8080");
+
+
+// informando a página HTML que será vista pelo usuário
+var express = require("express");
+
+var router = express.Router();
+
+router.use(function (req,res,next) {
+  console.log("/" + req.method);
+  next();
+});
+
+app.use("/",router);
+
+app.use(express.static(__dirname + '/views/'));
+
+// quando alguém conectar com o servidor por meio de socket
+io.on('connection', function (socket) {
+    
+  	// lê o conteúdo da porta serial
+	parser.on('data', function (data) {
+		// enviar resposta da leitura do cartão para a página web
+    	socket.emit('respostaTemp', data);
+      	socket.emit('respostaUmi', data);
+      	socket.emit('respostaLum', data);
+	});
 });
