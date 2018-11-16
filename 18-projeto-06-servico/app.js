9196fad9138033023ae864fd81041ca3c46b9ee2
Orlewilson B. Maia <orlewilson.maia@gmail.com>
2018-11-16 00:19:08 -0400

    atualizacao do projeto 06 e projeto 06 servico]

--- 18-projeto-06-servico/app.js
+++ 18-projeto-06-servico/app.js
@@ -3,22 +3,74 @@
   Facilitador:       Prof. Orlewilson Bentes Maia
   Data Criacao:      15/11/2018
   Data Atualizacao:  16/11/2018
-  Descricao:         Projeto 03: Serviço para alertar sobre 
-                     proximidade de objetos
+  Descricao:         Projeto 06: Serviço para mostrar informações
+                     de uma mini estação meteorológica
 
 */
+// importando bibliotecas
+// biblioteca para trabalhar com páginas web
+var app = require('express')();
+
+// biblioteca para saber ip da máquina
+var ip = require("ip");  
 
 // importando bibliotecas
+// para acesso serial a placa Arduino
+var SerialPort = require('serialport');
+const Readline = require('@serialport/parser-readline')
 
-// biblioteca para comunicar com o Arduino
-var five = require("johnny-five");  
+// abre a porta de comunicação
+var port = new SerialPort('COM14', {
+  baudRate: 9600
+});
 
-// informando a porta de comunicação
-var board = new five.Board({port: "COM14"});
- 
 // variáveis
 
-// quando a placa estiver pronta, execute.
-board.on("ready", function() {  
-   
+// formatador de conteúdo
+const parser = port.pipe(new Readline({delimiter: '\r\n'}))
+
+// armazena valores temperatura, umidade e luminosidade, respectivamente
+var temp, umi, lumi;
+
+// servidor escutando na porta 8080
+app.listen(8080);
+  
+console.log("Digite no seu navegador http://"+ ip.address() + ":8080");
+console.log("Saber valor temperatura: /temp");
+console.log("Saber valor umidade: /umi");
+console.log("Saber valor luminosidade: /lumi");
+console.log("Saber valor todos: /todos");
+
+
+// lê o conteúdo da porta serial
+parser.on('data', function (data) {
+
+  // mostra os dados no console lidos pela porta serial do arduino
+  console.log(data);
+
+  // obtem o arquivo JSON e ler cada parâmetro
+  var obj = JSON.parse(data);
+  temp = obj.Temperatura;
+  umi = obj.Umidade;
+  lumi = obj.Luminosidade;
+});
+
+// aguardando chamada /todos 
+app.get('/todos', function (req, res) {
+  res.json({temperatura: temp, umidade: umi, luminosidade: lumi});
+});
+
+// aguardando chamada /temp 
+app.get('/temp', function (req, res) {
+  res.json({temperatura: temp});
+});
+
+// aguardando chamada /umi 
+app.get('/umi', function (req, res) {
+  res.json({umidade: umi});
+});
+
+// aguardando chamada /lumi 
+app.get('/lumi', function (req, res) {
+  res.json({luminosidade: lumi});
 });
